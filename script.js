4a33ed6cdc1249f96c8a12878a23b0097223f468
Edward_375 <rotaruedward@yahoo.com>
2022-02-02 17:08:30 +0200

    change js and css

--- script.js
+++ script.js
@@ -7,23 +7,28 @@ const month = document.querySelector(".month")
 const year = document.querySelector(".year")
 const cvv = document.querySelector(".cvv")
 
-numberCard.textContent = "#### #### #### ####"
+const defaultContent = "#### #### #### ####"
 cardNumber.addEventListener("input", ev => {
     // console.log(ev.target.value);
     const valueCardNumber = ev.target.value
-    console.log(ev.target.value);
+    // console.log(ev.target.value);
 
-    for (let i = 0; i < valueCardNumber.length; i++) {
-        let a = valueCardNumber[i];
-        numberCard.textContent = numberCard.textContent + "array[" + i + "].value= "
-            + a.value + " ";
-        console.log(valueCardNumber[i], "valoarea input");
-        console.log(numberCard.textContent, "valoarea care trebie sa fie adaugata");
 
+    console.log(numberCard)
+    console.log(valueCardNumber)
+
+ const   objIndex = valueCardNumber.split((index => index.value === 1));
+    console.log(objIndex, "aaa");
+    if (valueCardNumber.length < 4){
+        numberCard.innerHTML += valueCardNumber[valueCardNumber.length -1];
+    }
+    if (valueCardNumber.length >= 5 && valueCardNumber.length <= 13){
+        numberCard.innerHTML += "#";
+    }
+    if (valueCardNumber.length > 13 && valueCardNumber.length <= 16){
+        numberCard.innerHTML += valueCardNumber[valueCardNumber.length -1];
     }
 
-    document.getElementById("par").innerHTML = numberCard.value;
-    document.getElementById("po").innerHTML = "input";
 })
     // if (arr.length){
     //     // const valueNumber =`${arr[0]}${arr[1]}${arr[2]}${arr[3]} ${arr[4]}${arr[5]}${arr[6]}${arr[7]} ${arr[8]}${arr[9]}${arr[10]}${arr[11]} ${arr[12]}${arr[13]}${arr[14]}${arr[15]}`
