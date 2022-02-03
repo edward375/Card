771fb4b898fdafc7405db14b6851f2508f3f064e
Edward_375 <rotaruedward@yahoo.com>
2022-02-03 10:17:49 +0200

    add event for inpu

--- script.js
+++ script.js
@@ -17,18 +17,30 @@ cardNumber.addEventListener("input", ev => {
     console.log(numberCard)
     console.log(valueCardNumber)
 
- const   objIndex = valueCardNumber.split((index => index.value === 1));
+ const   objIndex = valueCardNumber.split((index => index.id === 1));
     console.log(objIndex, "aaa");
-    if (valueCardNumber.length < 4){
-        numberCard.innerHTML += valueCardNumber[valueCardNumber.length -1];
-    }
-    if (valueCardNumber.length >= 5 && valueCardNumber.length <= 13){
-        numberCard.innerHTML += "#";
-    }
-    if (valueCardNumber.length > 13 && valueCardNumber.length <= 16){
-        numberCard.innerHTML += valueCardNumber[valueCardNumber.length -1];
-    }
+    // if (valueCardNumber.length < 4){
+    //     numberCard.innerHTML += valueCardNumber[valueCardNumber.length -1];
+    // }
+    // if (valueCardNumber.length >= 5 && valueCardNumber.length <= 13){
+    //     numberCard.innerHTML += "#";
+    // }
+    // if (valueCardNumber.length > 13 && valueCardNumber.length <= 16){
+    //     numberCard.innerHTML += valueCardNumber[valueCardNumber.length -1];
+    // }
 
+    objIndex.forEach((elem) =>{
+        console.log(elem);
+        if (elem.length < 4){
+            numberCard.innerHTML += elem[elem.length -1];
+        }
+        if (elem.length >= 5 && elem.length <= 13){
+            numberCard.innerHTML += "#";
+        }
+        if (elem.length > 13 && elem.length <= 16){
+            numberCard.innerHTML += elem[elem.length -1];
+        }
+    })
 })
     // if (arr.length){
     //     // const valueNumber =`${arr[0]}${arr[1]}${arr[2]}${arr[3]} ${arr[4]}${arr[5]}${arr[6]}${arr[7]} ${arr[8]}${arr[9]}${arr[10]}${arr[11]} ${arr[12]}${arr[13]}${arr[14]}${arr[15]}`
